59b5b683045157e01feed0f286c95f773e29a8a4
db400tea <db400tea@FULL1-05-01>
2023-05-10 17:48:54 +0900

    cjeonjae day1

---
+++ SampleTest/src/main/java/TestJava.java
@@ -0,0 +1,12 @@
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("반갑습니다.");
+		System.out.println("Hello~~");
+
+	}
+
+}
